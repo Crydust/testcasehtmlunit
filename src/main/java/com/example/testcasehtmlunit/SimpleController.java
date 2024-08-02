2365fbf2e29b94b893ed33eb1709f6eb48a31453
Kristof Neirynck <k.neirynck@gmail.com>
2024-08-02 15:32:41 +0200

    Add more testcases

--- src/main/java/com/example/testcasehtmlunit/SimpleController.java
+++ src/main/java/com/example/testcasehtmlunit/SimpleController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import static java.util.Objects.requireNonNullElse;
+import static org.springframework.web.util.HtmlUtils.htmlEscape;
 
 @Controller
 @RequestMapping("/")
@@ -19,7 +20,7 @@ public class SimpleController {
             </head>
             <body>
                 <h1>Minimal testcase query parameters ignored in post request by HtmlUnitRequestBuilder</h1>
-                <p>You submitted form <b id="submittedForm">%s</b>.</p>
+                <p>You submitted form <b id="submittedForm">%s</b>, the values of 'x' are: <b id="valuesOfX">%s</b>.</p>
                 <form action="?form=1" method="post" id="form1">
                     <p><button type="submit">Submit form <b>1</b> (form has query parameter in action)</button></p>
                 </form>
@@ -35,14 +36,36 @@ public class SimpleController {
                 <form action="?form=5" method="post" id="form5" enctype="multipart/form-data">
                     <p><button type="submit" name="dummy" value="foo">Submit form <b>5</b> (form has query parameter in action, enctype is multipart/form-data)</button></p>
                 </form>
+                <form action="?form=6&amp;x=query" method="post" id="form6">
+                    <p>
+                        <input type="hidden" name="x" value="hidden">
+                        <button type="submit" name="x" value="button">Submit form <b>6</b> (form has query parameter in action, multiple values for 'x')</button>
+                    </p>
+                </form>
+                <form action="?" method="post" id="form7">
+                    <p>
+                        <input type="checkbox" name="x" value="checkbox1" checked="checked"> x = checkbox1,
+                        <input type="checkbox" name="x" value="checkbox2" checked="checked"> x = checkbox2,
+                        <input type="checkbox" name="x" value="checkbox3" checked="checked"> x = checkbox3<br>
+                        <button type="submit" name="form" value="7">Submit form <b>7</b> (simple form, multiple values for 'x' checkbox)</button>
+                    </p>
+                </form>
             </body>
             </html>
             """;
 
     @GetMapping
     @ResponseBody
-    public String doGet(@SessionAttribute(name = "submittedForm", required = false) String submittedForm, HttpSession session) {
-        return HTML_TEMPLATE.formatted(requireNonNullElse(submittedForm, "none"));
+    public String doGet(
+            @SessionAttribute(name = "submittedForm", required = false) String submittedForm,
+            @SessionAttribute(name = "valuesOfX", required = false) String valuesOfX,
+            HttpSession session) {
+        session.removeAttribute("submittedForm");
+        session.removeAttribute("valuesOfX");
+        return HTML_TEMPLATE.formatted(
+                htmlEscape(requireNonNullElse(submittedForm, "none")),
+                htmlEscape(requireNonNullElse(valuesOfX, "none"))
+        );
     }
 
     @PostMapping(params = "form=1")
@@ -72,6 +95,30 @@ public class SimpleController {
     @PostMapping(params = "form=5")
     public String handleForm5(String form, HttpSession session) {
         session.setAttribute("submittedForm", form);
+        return "redirect:/";
+    }
+
+    @PostMapping(params = "form=6")
+    public String handleForm6(
+            String form,
+            @RequestParam(name = "x", required = false) String[] x,
+            HttpSession session) {
+        session.setAttribute("submittedForm", form);
+        if (x != null) {
+            session.setAttribute("valuesOfX", String.join(", ", x));
+        }
+        return "redirect:/";
+    }
+
+    @PostMapping(params = "form=7")
+    public String handleForm7(
+            String form,
+            @RequestParam(name = "x", required = false) String[] x,
+            HttpSession session) {
+        session.setAttribute("submittedForm", form);
+        if (x != null) {
+            session.setAttribute("valuesOfX", String.join(", ", x));
+        }
         return "redirect:/";
     }
 
--- src/test/java/com/example/testcasehtmlunit/SimpleControllerTest.java
+++ src/test/java/com/example/testcasehtmlunit/SimpleControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 /**
  * When using HtmlUnit ...
@@ -34,7 +35,6 @@ public class SimpleControllerTest {
 
     @Test
     void shouldHandleForm1() {
-        // fails with HtmlUnitRequestBuilder
         driver.get("http://localhost:8080/");
 
         driver.findElement(By.cssSelector("#form1 button")).click();
@@ -45,7 +45,6 @@ public class SimpleControllerTest {
 
     @Test
     void shouldHandleForm2() {
-        // fails with HtmlUnitRequestBuilder
         driver.get("http://localhost:8080/");
 
         driver.findElement(By.cssSelector("#form2 button")).click();
@@ -66,7 +65,6 @@ public class SimpleControllerTest {
 
     @Test
     void shouldHandleForm4() {
-        // fails with HtmlUnitRequestBuilder
         driver.get("http://localhost:8080/");
 
         driver.findElement(By.cssSelector("#form4 button")).click();
@@ -77,13 +75,42 @@ public class SimpleControllerTest {
 
     @Test
     void shouldHandleForm5() {
-        // fails with HtmlUnitRequestBuilder
         driver.get("http://localhost:8080/");
 
         driver.findElement(By.cssSelector("#form5 button")).click();
 
         String submittedForm = driver.findElement(By.cssSelector("#submittedForm")).getText();
         assertThat(submittedForm, is("5"));
+    }
+
+    @Test
+    void shouldHandleForm6() {
+        // fails with HtmlUnitRequestBuilder
+        driver.get("http://localhost:8080/");
+
+        driver.findElement(By.cssSelector("#form6 button")).click();
+
+        String submittedForm = driver.findElement(By.cssSelector("#submittedForm")).getText();
+        String valuesOfX = driver.findElement(By.cssSelector("#valuesOfX")).getText();
+        assertAll(
+                () -> assertThat(submittedForm, is("6")),
+                () -> assertThat(valuesOfX, is("query, hidden, button"))
+        );
+    }
+
+    @Test
+    void shouldHandleForm7() {
+        // fails with HtmlUnitRequestBuilder
+        driver.get("http://localhost:8080/");
+
+        driver.findElement(By.cssSelector("#form7 button")).click();
+
+        String submittedForm = driver.findElement(By.cssSelector("#submittedForm")).getText();
+        String valuesOfX = driver.findElement(By.cssSelector("#valuesOfX")).getText();
+        assertAll(
+                () -> assertThat(submittedForm, is("7")),
+                () -> assertThat(valuesOfX, is("checkbox1, checkbox2, checkbox3"))
+        );
     }
 
 }
